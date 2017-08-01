07081f0f1b12b7111184c41506457d42f2762984
Andrew Crozier <wacrozier@gmail.com>
2017-08-02 00:39:44 +0100

    Test file read and chunking behaviour

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,5 +1,8 @@
 import pytest
+from mock import Mock, MagicMock, call
+from six.moves import builtins
 
+import resumable.file
 from resumable.file import LazyLoadChunkableFile
 
 
@@ -21,11 +24,55 @@ def sample_file(tmpdir):
     return path
 
 
+@pytest.fixture
+def mock_lock(monkeypatch):
+    lock = Mock(__enter__=MagicMock(), __exit__=MagicMock())
+    monkeypatch.setattr(resumable.file, 'Lock', Mock(return_value=lock))
+    return lock
+
+
+@pytest.fixture
+def mock_open(monkeypatch):
+    file = Mock(seek=MagicMock(), read=MagicMock())
+    open = MagicMock(return_value=file)
+    monkeypatch.setattr(builtins, 'open', open)
+    return open
+
+
 def test_file(sample_file):
     file = LazyLoadChunkableFile(sample_file, TEST_CHUNK_SIZE)
     assert file.path == sample_file
     assert file.chunk_size == TEST_CHUNK_SIZE
     assert file.size == len(SAMPLE_CONTENT)
+
+
+def test_read_bytes(sample_file):
+    file = LazyLoadChunkableFile(sample_file, TEST_CHUNK_SIZE)
+    assert file.read_bytes(2, 10) == SAMPLE_CONTENT[2:12]
+
+
+def test_read_bytes_lock(sample_file, mock_lock, mock_open):
+
+    # Collect all lock and file related calls together
+    manager = Mock(
+        lock=mock_lock,
+        open=mock_open,
+        file=mock_open.return_value
+    )
+
+    # Creating a file should cause the file to be opened in rb mode
+    file = LazyLoadChunkableFile(sample_file, TEST_CHUNK_SIZE)
+    assert manager.mock_calls == [call.open(sample_file, 'rb')]
+    manager.reset_mock()
+
+    # Reading bytes from the file should happen in the contect of the lock
+    file.read_bytes(2, 10)
+    assert manager.mock_calls == [
+        call.lock.__enter__(),
+        call.file.seek(2),
+        call.file.read(10),
+        call.lock.__exit__(None, None, None)
+    ]
 
 
 def test_chunking(sample_file):
