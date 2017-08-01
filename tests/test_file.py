84812010389ad98ec2a7ffdd04a5dc97c43b0aba
Andrew Crozier <wacrozier@gmail.com>
2017-08-02 00:02:07 +0100

    Added test for file api

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,6 +1,5 @@
 import pytest
 
-
 from resumable.file import LazyLoadChunkableFile
 
 
@@ -22,8 +21,15 @@ def sample_file(tmpdir):
     return path
 
 
+def test_file(sample_file):
+    file = LazyLoadChunkableFile(sample_file, TEST_CHUNK_SIZE)
+    assert file.path == sample_file
+    assert file.chunk_size == TEST_CHUNK_SIZE
+    assert file.size == len(SAMPLE_CONTENT)
+
+
 def test_chunking(sample_file):
-    file = LazyLoadChunkableFile(sample_file, 10)
+    file = LazyLoadChunkableFile(sample_file, TEST_CHUNK_SIZE)
     assert len(list(file.chunks)) == len(list(expected_chunk_data()))
     for chunk, expected_data in zip(file.chunks, expected_chunk_data()):
         assert chunk.data == expected_data
