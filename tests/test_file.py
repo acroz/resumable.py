39b6c58b211b90ec1615455588e2167f2c090b11
Andrew Crozier <wacrozier@gmail.com>
2017-08-01 23:54:45 +0100

    Added first test, for file chunking

---
+++ tests/test_file.py
@@ -0,0 +1,29 @@
+import pytest
+
+
+from resumable.file import LazyLoadChunkableFile
+
+
+SAMPLE_CONTENT = b"sample content afsdfas"
+TEST_CHUNK_SIZE = 10
+
+
+def expected_chunk_data():
+    start = 0
+    while start < len(SAMPLE_CONTENT):
+        yield SAMPLE_CONTENT[start:start+TEST_CHUNK_SIZE]
+        start += TEST_CHUNK_SIZE
+
+
+@pytest.fixture
+def sample_file(tmpdir):
+    path = tmpdir.join('sample-file.txt')
+    path.write(SAMPLE_CONTENT)
+    return path
+
+
+def test_chunking(sample_file):
+    file = LazyLoadChunkableFile(sample_file, 10)
+    assert len(list(file.chunks)) == len(list(expected_chunk_data()))
+    for chunk, expected_data in zip(file.chunks, expected_chunk_data()):
+        assert chunk.data == expected_data
