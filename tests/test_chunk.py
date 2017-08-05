16a5310dd5063416d0144b6e0134a6290205aaaa
Andrew Crozier <wacrozier@gmail.com>
2017-08-05 14:41:55 +0100

    Added tests for chunk test method

--- tests/test_chunk.py
+++ tests/test_chunk.py
@@ -1,6 +1,7 @@
-from mock import Mock
+from mock import Mock, MagicMock, patch
+import pytest
 
-from resumable.core import ResumableChunk
+from resumable.core import ResumableChunk, ResumableChunkState, ResumableSignal
 
 
 def test_query():
@@ -13,3 +14,33 @@ def test_query():
         'resumableChunkNumber': 4,
         'resumableCurrentChunkSize': 100
     }
+
+
+@pytest.mark.parametrize('get_response_code, expected_state', [
+    (404, ResumableChunkState.QUEUED),
+    (200, ResumableChunkState.DONE)
+])
+def test_test(get_response_code, expected_state):
+    mock_target = 'https://example.com/upload'
+    mock_query = {'query': 'foo'}
+    mock_headers = {'header': 'bar'}
+    mock_send_signal = MagicMock()
+
+    requests_get_mock = MagicMock(
+        return_value=Mock(status_code=get_response_code)
+    )
+
+    with patch.object(ResumableChunk, 'query', mock_query), \
+            patch.object(ResumableChunk, 'send_signal', mock_send_signal):
+        chunk = ResumableChunk(Mock(), Mock())
+        with patch('requests.get', requests_get_mock):
+            chunk.test(mock_target, mock_headers)
+
+    requests_get_mock.assert_called_once_with(
+        mock_target, headers=mock_headers, data=mock_query
+    )
+    assert chunk.state == expected_state
+    if expected_state == ResumableChunkState.DONE:
+        mock_send_signal.assert_called_once_with(
+            ResumableSignal.CHUNK_COMPLETED
+        )
