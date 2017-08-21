8356e49d53683b0161aa70f1646282d19a08d9ff
Andrew Crozier <wacrozier@gmail.com>
2017-08-22 00:43:51 +0100

    Removed unused class

--- resumable/util.py
+++ resumable/util.py
@@ -21,20 +21,6 @@ class CallbackMixin(object):
             target.send_signal(signal)
 
 
-class FixedUrlSession(object):
-    """A simple wrapper for requests.Session that fixes the URL."""
-
-    def __init__(self, session, url):
-        self.session = session
-        self.url = url
-
-    def get(self, *args, **kwargs):
-        return self.session.get(self.url, *args, **kwargs)
-
-    def post(self, *args, **kwargs):
-        return self.session.post(self.url, *args, **kwargs)
-
-
 class Config(object):
 
     def __init__(self, **kwargs):
