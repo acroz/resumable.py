c10e5b3fde26515315449d194e2da1d9e39815cd
Andrew Crozier <wacrozier@gmail.com>
2017-11-24 01:18:27 +0000

    Factor chunking logic out of class

--- resumable/file.py
+++ resumable/file.py
@@ -6,6 +6,26 @@ from collections import namedtuple
 FileChunk = namedtuple('FileChunk', ['index', 'size', 'read'])
 
 
+def build_chunks(read_bytes, file_size, chunk_size):
+
+    chunks = []
+
+    index = 0
+    start = 0
+
+    while start < file_size:
+        end = min(start + chunk_size, file_size)
+        size = end - start
+
+        chunk = FileChunk(index, size, lambda: read_bytes(start, size))
+        chunks.append(chunk)
+
+        index += 1
+        start += chunk_size
+
+    return chunks
+
+
 class LazyLoadChunkableFile(object):
 
     def __init__(self, path, chunk_size):
@@ -17,7 +37,7 @@ class LazyLoadChunkableFile(object):
         self._fp = open(self.path, 'rb')
         self._fp_lock = Lock()
 
-        self._chunks = None
+        self.chunks = build_chunks(self._read_bytes, self.size, chunk_size)
 
     def close(self):
         self._fp.close()
@@ -26,21 +46,3 @@ class LazyLoadChunkableFile(object):
         with self._fp_lock:
             self._fp.seek(start)
             return self._fp.read(num_bytes)
-
-    @property
-    def chunks(self):
-        if self._chunks is not None:
-            return self._chunks
-        self._chunks = []
-        index = 0
-        start = 0
-        while start < self.size:
-            end = min(start + self.chunk_size, self.size)
-            size = end - start
-            chunk = FileChunk(
-                index, size, lambda: self._read_bytes(start, size)
-            )
-            self._chunks.append(chunk)
-            index += 1
-            start += self.chunk_size
-        return self._chunks
