01448920020e95acbb1fa1db50bdc5a81232fdc6
Andrew Crozier <wacrozier@gmail.com>
2017-12-21 16:42:42 +0000

    Include long description using README

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
+import os
 from setuptools import setup
+
+
+def source_root():
+    """Get the root of the source."""
+    return os.path.abspath(os.path.dirname(__file__))
+
+
+with open(os.path.join(source_root(), 'README.rst')) as fp:
+    LONG_DESCRIPTION = fp.read()
 
 
 setup(
     name='resumable',
     version='0.1.0',
-    description='Chunked upload to a server supporting resumable.js',
+    description='Python client for upload to a server supporting resumable.js',
+    long_description=LONG_DESCRIPTION,
     url='https://acroz.github.com/acroz/resumable.py',
     author='Andrew Crozier',
     author_email='wacrozier@gmail.com',
