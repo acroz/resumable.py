d47f14fd4b108680b84d762af3becff5b1fcb95c
Andrew Crozier <wacrozier@gmail.com>
2017-12-21 17:38:52 +0000

    Read the version from the version module

--- setup.py
+++ setup.py
@@ -7,15 +7,27 @@ def source_root():
     return os.path.abspath(os.path.dirname(__file__))
 
 
-with open(os.path.join(source_root(), 'README.rst')) as fp:
-    LONG_DESCRIPTION = fp.read()
+def read_version():
+    """Read the version from the resumable.version module."""
+    filename = os.path.join(source_root(), 'resumable/version.py')
+    with open(filename) as fp:
+        namespace = {}
+        exec(fp.read(), namespace)  # pylint: disable=exec-used
+        return namespace['__version__']
+
+
+def read_long_description():
+    """Read the README file."""
+    filename = os.path.join(source_root(), 'README.rst')
+    with open(filename) as fp:
+        return fp.read()
 
 
 setup(
     name='resumable',
-    version='0.1.0',
+    version=read_version(),
     description='Python client for upload to a server supporting resumable.js',
-    long_description=LONG_DESCRIPTION,
+    long_description=read_long_description(),
     url='https://acroz.github.com/acroz/resumable.py',
     author='Andrew Crozier',
     author_email='wacrozier@gmail.com',
